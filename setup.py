9bada56d73b8b786f0a7f276e4990b8b914e62df
mlgallardo <mikhag@uw.edu>
2019-07-22 13:54:19 -0700

    upload get_dcmag

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='get_dcmag',
+      version='0.1',
+      description='Get DC mag',
+      url='n/a',
+      author='MG',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['get_dcmag'],
+      install_requires=[
+          'markdown',
+		  'numpy',
+		  'pandas'		  
+      ],
+      zip_safe=False)
+
