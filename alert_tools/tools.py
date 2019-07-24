4260fea4874cebcd132856f9773709ba97d31e0e
mlgallardo <52393450+mlgallardo@users.noreply.github.com>
2019-07-24 13:45:44 -0700

    Update tools.py

--- alert_tools/tools.py
+++ alert_tools/tools.py
@@ -40,6 +40,8 @@ def dcmag(dflc, match_radius_arcsec=1.5, star_galaxy_threshold = 0.4,band=2):
             (10**(-0.4*dflc['magpsf']) * dflc['sigmapsf'])**2.) / 10**(-0.4*dflc['magnr']) + dflc['sign'] * 10**(-0.4*dflc['magpsf']) #u
         dflc['dc_mag_ulim'] = -2.5 * np.log10(10**(-0.4*dflc['magnr']) + 10**(-0.4*dflc['diffmaglim']))   #v
         dflc['dc_mag_llim'] = -2.5 * np.log10(10**(-0.4*dflc['magnr']) - 10**(-0.4*dflc['diffmaglim']))   #v2
+	
+	return dflc
         
 
         
